aef42d173ca26e1bb8d77acea75f99e333154aa6
whimsicalartist123 <nawalgaria.utkarsh8@gmail.com>
2020-12-07 16:44:21 +0530

    added custom user model

---
+++ apps/users/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+from .models import Customer, Seller, CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+@admin.register(Customer)
+class CustomerAdmin(ModelAdmin):
+    pass
+
+@admin.register(Seller)
+class SellerAdmin(ModelAdmin):
+    pass
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ('email', 'name', 'is_customer',)
+    list_filter = ('email', 'name', 'is_customer',)
+    fieldsets = (
+        (None, {'fields': ('name', 'email', 'password',)}),
+        ("Seller/Customer", {'fields': ('is_customer',)}),
+        ("Permissions", {'fields': ('is_staff', 'is_active',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name', 'email', 'password1', 'password2', 'is_staff', 'is_active', 'is_customer', 'is_seller',)}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    
---
+++ apps/users/managers.py
@@ -0,0 +1,41 @@
+from typing import Any, Optional
+from django.contrib.auth.base_user import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(
+        self, 
+        name: str, 
+        email: str,
+        password: Optional[str], 
+        **extra_fields: Any):
+        
+        if not email:
+            raise ValueError("The email must be set")
+        if not name:
+            raise ValueError("Name should be set")
+
+        user = self.model(
+            name=name,
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, name: str, email: str, password: Optional[str], **extra_fields: Any):
+
+        extra_fields.is_customer = False
+        user = self.create_user(
+            name,
+            email,
+            password=password,
+            **extra_fields
+        )
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
