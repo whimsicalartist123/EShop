17795017481062cfe18aaa683e40db304131e5da
whimsicalartist123 <nawalgaria.utkarsh8@gmail.com>
2021-02-18 22:06:25 +0530

    minor changes in the checkout view

--- apps/order/views.py
+++ apps/order/views.py
@@ -1,7 +1,4 @@
-from pprint import pprint
-from decimal import Decimal
-
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import View
 
 from apps.cart.cart import Cart
@@ -13,6 +10,9 @@ from .forms import CheckoutForm
 
 class CheckoutView(View):
     def get(self, *args, **kwargs):
+        cart = Cart(self.request)
+        if not cart:
+            return redirect('cart:detail')
         form = CheckoutForm()
         context = {
             "checkout_form": form
@@ -21,10 +21,11 @@ class CheckoutView(View):
         return render(self.request, "order/checkout.html", context)
     
     def post(self, *args, **kwargs):
+
         form = CheckoutForm(self.request.POST or None)
         cart = Cart(self.request)
+
         if form.is_valid():
-            pprint(form.cleaned_data)
             data = form.cleaned_data
 
             # Create new address mdoel
@@ -40,14 +41,12 @@ class CheckoutView(View):
                 last_name=data.get('last_name'),
                 address=address,
                 user_email=data.get('user_email'),
-                shipping_price=data.get('shipping_price'),
-                total_order_amount=cart.get_total_price()
+                total_order_amount=cart.get_total_price(),
+                shipping_price=data.get('shipping_choices')
             )
             
             # Create Order Line Items.
             for item in cart:
-                print(item)
-
                 OrderLineItem.objects.create(
                     order=new_order,
                     product=item['product'],
@@ -55,14 +54,12 @@ class CheckoutView(View):
                     quantity=int(item['quantity'])
                 )
 
-            if self.request.user.is_authenticated:
-                if self.request.user.is_customer:
-                    new_order.customer = self.request.user
-                    address.customer = self.request.user
+            if self.request.user.is_authenticated and self.request.user.is_customer:
+                new_order.customer = self.request.user
+                address.customer = self.request.user
 
             new_order.save()
             address.save()
 
             cart.clear()
-
-            return render(self.request, "order/created.html", {"order": new_order})
+            return render(self.request, "order/created.html")
