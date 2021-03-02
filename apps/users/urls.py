cddeea04fb35db6f4726a37cf2d916cdde3f600c
whimsicalartist123 <nawalgaria.utkarsh8@gmail.com>
2021-03-02 10:41:32 +0530

    added user dashboard url and views

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from .views import index
+
 app_name = 'user'
 
 urlpatterns = [
-    # path('/')
+    path('', index, name="user_dashboard")
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def index(request):
+    return render(
+        request,
+        "dashboard/dashboard.html",
+        {}
+    )
